92335f398a8278d4bc9e3023fa24e5f8fddb8d78
noguchitakuya <takuya.noguchi@rakus-partners.co.jp>
2022-02-07 15:20:39 +0900

    初級演習課題１の実施

---
+++ src/views/Ex01.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div>名前：{{ name }}</div>
+    <div>年齢：{{ age }}</div>
+    <div>10年後の年齢：{{ ageplus10 }}</div>
+    <div>
+      <span v-for="hob of hobby" :key="hob">{{ hob }}</span>
+    </div>
+    <div></div>
+    <div>
+      <a href="#">自社ページリンク</a>
+    </div>
+    <div>
+      <img src="/pagu.jpg" alt="" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+@Component
+export default class XXXComponent extends Vue {
+  private name = "野口拓也";
+  private age = 24;
+  private hobby = ["スイム", "バイク", "ラン"];
+
+  get ageplus10(): number {
+    return this.age + 10;
+  }
+}
+</script>
+
+<style scoped></style>
